d17c36bcd220eb5c373b094b75e8d32fa818cd61
Ihan Kang <ihankang11@gmail.com>
2020-06-07 10:29:59 -0700

    done

--- gary/singleton/khala/gary_khala.py
+++ gary/singleton/khala/gary_khala.py
@@ -3,3 +3,9 @@ class GaryCommand:
     def reply(cls, text_in):
         if text_in == "hello":
             return "hello!!!"
+
+        if text_in == "hydrogen":
+            return "name:hydrogen, proton:1, neutron:0, weight:1"
+
+        if text_in == "H":
+            return "name:hydrogen, proton:1, neutron:0, weight:1"
