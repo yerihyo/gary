19cda26b5bf8eaa4929c0c4bc7abf6dc374e11ad
Ihan Kang <ihankang11@gmail.com>
2020-06-07 11:44:07 -0700

    done

--- gary/singleton/khala/gary_khala.py
+++ gary/singleton/khala/gary_khala.py
@@ -4,8 +4,38 @@ class GaryCommand:
         if text_in == "hello":
             return "hello!!!"
 
-        if text_in == "hydrogen":
-            return "name:hydrogen, proton:1, neutron:0, weight:1"
+        if text_in in {"hydrogen","H"}:
+            return "name:hydrogen, proton:1, neutron:0, weight:1.0079"
 
-        if text_in == "H":
-            return "name:hydrogen, proton:1, neutron:0, weight:1"
+        if text_in == "ihan":
+            return "Dad is my master and ihan is stupid!!!"
+
+        if text_in == "dum":
+            return "Dum delicious daddy"
+
+        if text_in in {"zinc","Zn"}:
+            return "name:zinc, proton:30, neutron:35, weight:65.39"
+
+
+
+
+        if text_in == "mom":
+            return "moooooooooooooooooooooooooooooooooooooooooooooomy"
+
+        if text_in == "(   )":
+            return "space!!!"
+
+        if text_in in {"iron","Fe"}:
+            return "name:iron, proton:26, neutron:30, weight:55.845"
+
+        if text_in in {"manganese","Mn"}:
+            return "name:manganese, proton:25, neutron:30, weight:54.938"
+
+        if text_in in {"titanium","Ti"}:
+            return "name:titanium, proton:22, neutron:26, weight:47.867"
+
+        if text_in == "w4yufgi2ue2hyu":
+            return "w4yufgi2ue2hyu"
+
+        if text_in in {"iron","Fe"}:
+            return "name:iron, proton:26, neutron:30, weight:55.845"
